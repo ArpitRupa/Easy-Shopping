601ba25cb909a1f489e368758517d07740a4dea6
ArpitRupa <ar9cd@virginia.edu>
2023-10-02 16:15:57 -0400

    inital User model, Url controller, database configuration

---
+++ easyshopping/easyshopping/src/main/java/com/easyshopping/easyshopping/model/User.java
@@ -0,0 +1,13 @@
+package com.easyshopping.easyshopping.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+
+}
---
+++ easyshopping/shopping_db.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE IF NOT EXISTS shopping_db;
+USE shopping_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    username VARCHAR(25) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    rating DECIMAL(3, 2)
+);
+
+CREATE TABLE IF NOT EXISTS product_images (
+    image_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    image_data BLOB NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE IF NOT EXISTS product_reviews (
+    review_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    reviewer_id INT NOT NULL,
+    review_text TEXT NOT NULL,
+    rating INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (reviewer_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+	product_id INT NOT NULL,
+	buyer_id INT NOT NULL,
+	product_count INT NOT NULL,
+    date_time DATETIME NOT NULL,
+    shipping_address TEXT NOT NULL,
+    total_cost DECIMAL(10, 2) NOT NULL,
+	FOREIGN KEY (buyer_id) REFERENCES users(id),
+	FOREIGN KEY (product_id) REFERENCES products(id)
+);
