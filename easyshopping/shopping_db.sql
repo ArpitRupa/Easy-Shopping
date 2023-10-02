0e70e397d06c9f605578355642d1489896ad7d05
ArpitRupa <ar9cd@virginia.edu>
2023-10-02 18:18:38 -0400

    implemented User model and updated users table's username in SQL for uniqueness

--- easyshopping/shopping_db.sql
+++ easyshopping/shopping_db.sql
@@ -6,8 +6,9 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(30) NOT NULL,
-    username VARCHAR(25) NOT NULL,
+    username VARCHAR(25) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL
+
 );
 
 CREATE TABLE IF NOT EXISTS products (
--- easyshopping/src/main/java/com/fullstackshopping/easyshopping/model/User.java
+++ easyshopping/src/main/java/com/fullstackshopping/easyshopping/model/User.java
@@ -1,9 +1,6 @@
 package com.fullstackshopping.easyshopping.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 
@@ -11,13 +8,90 @@ import java.io.Serializable;
 @Entity
 public class User implements Serializable {
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
     private int id;
+
+    @Column(nullable = false, name = first_name)
     private String firstName;
+
+    @Column(nullable = false, name=last_name)
     private String lastName;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email, String username, String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId(){
+        return  this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString(){
+        return "User{" +
+                "\n\"id\"= \"" + this.id + "\"" +
+                "\n\"first name\"= \"" + this.firstName + "\"" +
+                "\n\"last name\"= \"" + this.lastName + "\"" +
+                "\n\"email\"= \"" + this.email + "\"" +
+                "\n\"username\"= \"" + this.username + "\"" +
+                "\n}";
+    }
 }
