ffb7482e3c6358a3fc5ba23f5894011164e747f2
ArpitRupa <ar9cd@virginia.edu>
2023-10-14 16:32:10 -0400

    initial angular set up

---
+++ easyshopping/src/main/resources/static/easyshopping/src/app/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private builder: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ easyshopping/src/main/resources/static/easyshopping/src/material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatTableModule } from '@angular/material/table'
+import { MatSortModule } from '@angular/material/sort'
+import { MatDialogModule } from '@angular/material/dialog'
+
+
+
+
+@NgModule({
+    exports: [
+        MatInputModule,
+        MatSelectModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatTableModule,
+        MatSortModule,
+        MatDialogModule
+    ]
+
+})
+export class MaterialModule {
+
+}
